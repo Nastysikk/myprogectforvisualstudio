3f273e88eb42b07dc2f3a776a434ef256e4a37b0
Nastia <anastasiiamarets@nltu.lviv.ua>
2021-11-30 19:46:26 +0200

    Add project files.

---
+++ ConsoleAppforgithub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppforgithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть ім'я: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введіть вік: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Ім'я: {name}  Вік: {age}");
+        }
+    }
+}
